ea5970c14afa08ad6c87ee7c94e9cf82ba2b0d60
Jonathan Dong <jonathan.dong@intel.com>
2015-07-01 12:29:55 +0800

    Renamed the dotfile path.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #DIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
-DIR="$HOME/.jondong"
+DIR="$HOME/.dotfiles"
 
 if [ ! -d "$DIR" ]; then
-  echo "Installing jondong's dotfiles for the first time."
+  echo "Installing dotfiles for the first time."
   git clone https:://github.com/jondong/dotfiles.git "$DIR"
   pushd "$DIR" > /dev/null
 else
-  echo "Already installed jondong's dotfiles, updating..."
+  echo "Already installed dotfiles, updating..."
   pushd "$DIR" > /dev/null
   git pull --rebase origin master
 fi
