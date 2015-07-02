3b8a93f0d94532be1cf41ecf842d71828f4554ea
Jonathan Dong <jonathan.dong@intel.com>
2015-07-02 11:00:02 +0800

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,123 @@
 #!/usr/bin/env bash
 
-#DIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
-DIR="$HOME/.dotfiles"
+#DOTFILES_ROOT="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+DOTFILES_ROOT="$HOME/.dotfiles"
 
-if [ ! -d "$DIR" ]; then
-  echo "Installing dotfiles for the first time."
-  git clone https:://github.com/jondong/dotfiles.git "$DIR"
-  pushd "$DIR" > /dev/null
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done
+}
+
+
+if [ ! -d "$DOTFILES_ROOT" ]; then
+  info "installing dotfiles for the first time."
+  git clone https:://github.com/jondong/dotfiles.git "$DOTFILES_ROOT"
+  pushd "$DOTFILES_ROOT" > /dev/null
+
+  install_dotfiles
 else
-  echo "Already installed dotfiles, updating..."
-  pushd "$DIR" > /dev/null
+  info "already installed dotfiles, updating..."
+  pushd "$DOTFILES_ROOT" > /dev/null
   git pull --rebase origin master
 fi
 
