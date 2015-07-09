3a01995147589d170f365fb0946e5e20ac37ee71
Jonathan Dong <jonathan.dong@intel.com>
2015-07-09 09:32:08 +0800

    Install gitup for linux.

--- linux/install.sh
+++ linux/install.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
 # Install necessary softwares for Linux here.
 
+# Install gitup from git-repo-updater.
+echo "Installing gitup..."
+git clone https://github.com/earwig/git-repo-updater.git /tmp/git-repo-updater
+pushd /tmp/git-repo-updater
+
+python setup.py install --user
+
+popd
