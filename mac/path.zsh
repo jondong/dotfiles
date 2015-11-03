12e3199d8cbf8e3e50fddfbd4d8dd1e2f11c855c
Jonathan Dong <jonathan.dong@intel.com>
2015-11-03 11:05:17 +0800

    Make sure path.zsh for Mac cannot be called in Linux.
    This is a temporary fix, will update soon.

--- mac/path.zsh
+++ mac/path.zsh
@@ -1,5 +1,7 @@
 # Append necessary path to PATH for Mac.
 
-# nvm configuration.
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+if [ "$(uname)" = 'Darwin' ]; then
+  # nvm configuration.
+  export NVM_DIR=~/.nvm
+  source $(brew --prefix nvm)/nvm.sh
+fi
