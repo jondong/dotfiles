c0c51a2b4143af496f280a2c226a4dfceae7ecb8
Jonathan Dong <jonathan.dong@intel.com>
2015-11-03 09:01:59 +0800

    Moved NVM configuration from path.zsh to mac/path.zsh

--- mac/path.zsh
+++ mac/path.zsh
@@ -1,1 +1,5 @@
 # Append necessary path to PATH for Mac.
+
+# nvm configuration.
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
--- path.zsh
+++ path.zsh
@@ -1,7 +1,3 @@
 # Append necessary path to PATH.
 
 source $ZSH/profile
-
-# nvm configuration.
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
