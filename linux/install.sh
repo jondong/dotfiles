95a9413443256ef14505808beb49446b709103eb
Jonathan Dong <jonathan.dong@intel.com>
2016-03-16 10:03:35 +0800

    Updated Linux install.

--- linux/install.sh
+++ linux/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Install necessary softwares for Linux here.
+sudo apt-get install aptitude
+
+sudo aptitude install vim git git-extras git-completion ruby-dev rubygems tmux terminator source-highlight mosh autojump xclip nodejs npm privoxy ssh tree expect cgdb gvim valgrind clang shellcheck tmux openssl gnutils gnutls-bin global cscope exuberant-ctags libgnome-keyring-dev unzip curl tar python-setuptools python-pip
+
+sudo gem install ghi
 
 # Install gitup from git-repo-updater.
 echo "Installing gitup..."
@@ -8,9 +13,10 @@ pushd /tmp/git-repo-updater
 python setup.py install --user
 popd
 
-sudo gem install ghi
-sudo apt-get install aptitude
-sodo apt-get install mosh
-sudo apt-get install xclip # or xsel. this is for tmux-yank plugin.
-sudo aptitude install autojump
-sudo aptitude install source-highlight
+node_module_path=$(npm config get prefix)
+if [ $node_module_path = '/usr/local' ]; then
+  sudo mkdir $node_module_path/node_modules
+  sudo chown -R $(whoami) $node_module_path/{lib/node_modules,bin,share}
+fi
+
+npm install -g coffee-script
