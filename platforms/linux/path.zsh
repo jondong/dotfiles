4b30042e8dc1aae17f08cd25e659cda2e25c75a1
Jonathan Dong <jonathan.dong@intel.com>
2016-10-17 12:59:26 +0800

    Remove source-highlight which causes hanging problem

--- platforms/linux/path.zsh
+++ platforms/linux/path.zsh
@@ -2,10 +2,3 @@
 
 source /usr/share/autojump/autojump.zsh
 
-if [ $(command_exists source-highlight) ]; then
-  export LESSOPEN="/usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-  export LESS=' -R '
-
-  alias ccat='/usr/share/source-highlight/src-hilite-lesspipe.sh'
-  alias cat=ccat
-fi
