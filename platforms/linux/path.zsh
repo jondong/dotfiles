0e50845e2da59ee122876ac99807c2da3c811d5b
Jonathan Dong <jonathan.dong@intel.com>
2016-10-17 13:17:38 +0800

    Move the platform specific codes into each platform's path.zsh

--- platforms/linux/path.zsh
+++ platforms/linux/path.zsh
@@ -2,3 +2,18 @@
 
 source /usr/share/autojump/autojump.zsh
 
+## For gitup setup.
+## gitup is a tool to sync multiple git repos in a single shot.
+## For more information please refer to: https://github.com/earwig/git-repo-updater
+append_path_if_exists "$HOME/.local/bin"
+
+## Ant setup
+ANT_BIN_PATH=$HOME/bin/apache-ant/bin
+prepend_path_if_exists "$ANT_BIN_PATH"
+
+## JDK setup
+if [ -d "$HOME/bin/jdk" ]; then
+  export JAVA_HOME=$HOME/bin/jdk
+  JDK_BIN_PATH=$JAVA_HOME/bin
+  prepend_path_if_exists "$JDK_BIN_PATH"
+fi
--- platforms/mac/path.zsh
+++ platforms/mac/path.zsh
@@ -4,3 +4,34 @@
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 alias cat='ccat'
+
+# autojump configuration.
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+
+# Path for python binaries
+append_path_if_exists "$HOME/Library/Python/2.7/bin"
+
+if [ -d /opt/local ]; then
+  # using Macports
+  EXTRA_LIB_PREFIX=/opt/local
+elif [ -d /usr/local ]; then
+  # using Homebrew
+  EXTRA_LIB_PREFIX=/usr/local
+fi
+
+if [ -z $EXTRA_LIB_PREFIX ]; then
+  exit
+fi
+export PATH=$EXTRA_LIB_PREFIX/bin:$EXTRA_LIB_PREFIX/sbin:$PATH
+export LD_LIBRARY_PATH=$EXTRA_LIB_PREFIX/lib:$LD_LIBRARY_PATH
+
+# MacPorts Bash shell command completion
+if [ -f $EXTRA_LIB_PREFIX/etc/bash_completion ]; then
+    . $EXTRA_LIB_PREFIX/etc/bash_completion
+    source $EXTRA_LIB_PREFIX/share/git/git-prompt.sh
+fi
+
+# Homebrew bash shell command completion
+if [ -d $EXTRA_LIB_PREFIX/etc/bash_completion.d ]; then
+    source $EXTRA_LIB_PREFIX/etc/bash_completion.d/git-extras
+fi
