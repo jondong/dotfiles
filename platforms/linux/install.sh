004fc9d9e2aae63766e16b1e1dca2d3e6975cd1f
Jonathan Dong <jonathan.dong@intel.com>
2016-10-18 13:39:30 +0800

    Added selection for production environment installation.

--- platforms/linux/install.sh
+++ platforms/linux/install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Install necessary softwares for Linux here.
-sudo apt-get -y install aptitude
+read -e -p "Install Linux for production? [y/N] " -n 1
+is_production=${REPLY:=n}
 
-# Install nodejs v6
+packages=(nodejs vim git tmux autojump xclip privoxy ssh tree vim-gtk3-py2 shellcheck openssl libgnome-keyring-dev unzip curl tar zsh)
+if [ ${is_production,,} = 'n' ]; then
+  echo "Install packages for development machine."
+  packages=("${packages[@]}" aptitude git-extras terminator mosh ruby ruby-dev source-highlight expect cgdb valgrind clang global cscope exuberant-ctags python-setuptools python-pip)
+else
+  echo "Install packages for production machine."
+fi
+
+# Setup nodejs v6 deb source
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-sudo aptitude -y install nodejs vim git git-extras git-completion ruby-dev rubygems tmux terminator source-highlight mosh autojump xclip npm privoxy ssh tree expect cgdb gvim valgrind clang shellcheck openssl gnutils gnutls-bin global cscope exuberant-ctags libgnome-keyring-dev unzip curl tar python-setuptools python-pip zsh
-
-sudo gem install ghi
-
-# Install gitup from git-repo-updater.
-echo "Installing gitup..."
-git clone https://github.com/earwig/git-repo-updater.git /tmp/git-repo-updater
-pushd /tmp/git-repo-updater
-python setup.py install --user
-popd
-
-sudo npm install -g coffee-script
+# Install necessary softwares for Linux here.
+sudo apt -y autoremove
+sudo apt -y upgrade
+sudo apt -y install "${packages[@]}"
 
 # Setup gnome keyring for git
 pushd /usr/share/doc/git/contrib/credential/gnome-keyring
@@ -31,7 +31,20 @@ pushd /usr/local/share/zsh
 sudo chmod -R 755 ./site-functions
 popd
 
-# setup crontab
-if [ -f "$HOME/.crontab" ]; then
-  crontab -u $(whoami) "$HOME/.crontab"
+if [ ${is_production,,} = 'n' ]; then
+  sudo gem install ghi
+
+  # Install gitup from git-repo-updater.
+  echo "Installing gitup..."
+  git clone https://github.com/earwig/git-repo-updater.git /tmp/git-repo-updater
+  pushd /tmp/git-repo-updater
+  python setup.py install --user
+  popd
+
+  sudo npm install -g coffee-script
+
+  # setup crontab
+  if [ -f "$HOME/.crontab" ]; then
+    crontab -u $(whoami) "$HOME/.crontab"
+  fi
 fi
