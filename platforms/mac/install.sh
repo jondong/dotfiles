d4bafd7238c31b9651874bbe42497f6e086732f9
simu <simu.dy@alibaba-inc.com>
2017-03-16 14:53:57 +0800

    Updated Mac installation

--- platforms/mac/install.sh
+++ platforms/mac/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #Install necessary softwares for Mac here.
 
+gem install teamocil cocoapods pygments.rb
+mkdir -p ~/.teamocil
+
 # Check if homebrew get installed.
 if [ ! $(command_exists brew) ]; then
   echo "  Installing homebrew for you..."
@@ -9,9 +12,6 @@ fi
 
 # Make sure we are using the lates homebrew.
 brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade --all
 
 # Install homebrew packages
 brew install coreutils git git-lfs git-extras
@@ -26,24 +26,23 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` and `/usr/local/bin/zsh`
 # to `/etc/shells` before running `chsh`.
 brew tap homebrew/versions
-brew install vim --with-client-server --override-system-vi
+#brew tap oclint/formulae
+brew install caskroom/cask/brew-cask
 brew install wget --with-iri
 brew install homebrew/dupes/grep homebrew/dupes/openssh
-brew install caskroom/cask/brew-cask
+brew cask install xquartz
+brew install vim --with-client-server --override-system-vi
 
-brew install bash zsh tmux bash-completion2 bash-git-prompt macvim cmake shellcheck xctool ccache chisel appledoc dark-mode ssh-copy-id tree npm autojump reattach-to-user-namespace gitup ag htop ccat mobile-shell global ctags ghi yarn icu4c
+brew install bash zsh tmux bash-completion2 bash-git-prompt macvim cmake shellcheck xctool ccache chisel appledoc dark-mode ssh-copy-id tree npm autojump reattach-to-user-namespace gitup ag htop ccat mobile-shell global ctags ghi yarn
 
-gem install teamocil gollum cocoapods
-mkdir -p ~/.teamocil
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Remove outdated versions from the cellar.
+brew cleanup
 
 # Install hexo for personal blog
 npm install -g hexo hexo-cli
 
-# Installations using cask
-brew cask install easysimbl oclint
-
 # Install simiki. for more information please refer to: http://simiki.org
 pip install simiki fabric
-
-# Remove outdated versions from the cellar.
-brew cleanup
