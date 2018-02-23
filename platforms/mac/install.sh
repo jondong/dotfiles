671768cae83aa300674470cd849368d619d06f6f
Jonathan Dong <jondong.community@outlook.com>
2018-02-23 13:09:32 +0800

    Install Rust using rustup

--- platforms/mac/install.sh
+++ platforms/mac/install.sh
@@ -33,7 +33,7 @@ brew install homebrew/dupes/grep homebrew/dupes/openssh
 brew cask install xquartz
 brew install vim --with-client-server --override-system-vi
 
-brew install bash zsh tmux bash-completion2 bash-git-prompt macvim cmake shellcheck xctool ccache chisel appledoc dark-mode ssh-copy-id tree npm autojump reattach-to-user-namespace gitup ag htop ccat mobile-shell global ctags ghi yarn aria2 mas gradle rust rustc-completion
+brew install bash zsh tmux bash-completion2 bash-git-prompt macvim cmake shellcheck xctool ccache chisel appledoc dark-mode ssh-copy-id tree npm autojump reattach-to-user-namespace gitup ag htop ccat mobile-shell global ctags ghi yarn aria2 mas gradle
 
 # Upgrade any already-installed formulae.
 brew upgrade --all
@@ -46,3 +46,6 @@ npm install -g hexo hexo-cli
 
 # Install simiki. for more information please refer to: http://simiki.org
 pip install simiki fabric
+
+# Install rust through rustup: https://www.rustup.rs/
+curl https://sh.rustup.rs -sSf | sh
