02054244dd26c36c7968ad4f89b64eb4a14fedc3
Jonathan Dong <jondong@apache.org>
2019-02-08 19:50:21 +0800

    Added backup_mac_app_list

---
+++ bin/backup_mac_app_list.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+sync_path="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Documents/backup/app-lists"
+total_apps_file="total_apps.conf"
+mas_apps_file="mas_apps.conf"
+brew_apps_file="brew_apps.conf"
+brew_cask_apps_file="brew_cask_apps.conf"
+
+# Backup app list.
+pushd "$sync_path"
+ls /Applications | sed 's/\.app//g' > "$total_apps_file"
+/usr/local/bin/mas list > "$mas_apps_file"
+/usr/local/bin/brew list > "$brew_apps_file"
+/usr/local/bin/brew cask list > "$brew_cask_apps_file"
+
+# Generate app installation script.
+install_script="app_installer.sh"
+echo -e "#!/usr/bin/env bash\n" > "$install_script"
+cat "$mas_apps_file" | sed "s/(.*)//g" | sed -Ee 's/([0-9]+) (.+)/mas install \1 #\2/g' >> "$install_script"
+echo -e "\nbrew install $(cat $brew_apps_file | tr '\n' ' ')" >> "$install_script"
+echo -e "\nbrew cask install $(cat $brew_cask_apps_file | tr '\n' ' ')" >> "$install_script"
+chmod +x "$install_script"
+
+# Execute it.
+popd
