43815691b475aaa906536374210ff597d3859bdd
Yan Dong <dongyan09@live.cn>
2024-11-01 15:23:25 +0800

    Update Mac install script

--- platforms/mac/install.sh
+++ platforms/mac/install.sh
@@ -1,63 +1,41 @@
 #!/usr/bin/env bash
 #Install necessary softwares for Mac here.
 
-# gem install teamocil pygments.rb
-# mkdir -p ~/.teamocil
-
-# Check if homebrew get installed.
-#if [ ! $(command_exists brew) ]; then
-#  echo "  Installing homebrew for you..."
-#  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#fi
-
 # Make sure we are using the lates homebrew.
 brew update
 
-# Install homebrew packages
-#brew install coreutils git git-lfs git-extras cocoapods tig
-#sudo ln -s $(brew --prefix)/bin/gsha256sum $(brew --prefix)/bin/sha256sum
+# Upgrade any already-installed formulae.
+brew upgrade --all
 
-# Install some other useful utilities like `sponge`.
-#brew install moreutils findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-#brew install gnu-sed --with-default-names
+# base system tools
+brew install bash zsh bash-completion2 bash-git-prompt zsh-competions reattach-to-user-namespace antigen findutils
 
-# Install Bash 4.
-# Note: don’t forget to add `$(brew --prefix)/bin/bash` and `$(brew --prefix)/bin/zsh`
-# to `/etc/shells` before running `chsh`.
-#brew tap homebrew/versions
-#brew tap oclint/formulae
-#brew tap caskroom/cask
-#brew tap buo/cask-upgrade
-#brew install wget --with-iri
-#brew install vim --with-client-server
+# utilities
+brew install nvim tmux zellij ssh-copy-id wget tree autojump ag btop bat mas z fd fzf ack prettyping mosh ncdu tldr trash rsync ripgrep highlight ca-certificates cat ccat duf the_silver_searcher
 
-brew install bash zsh tmux bash-completion2 bash-git-prompt shellcheck xctool ssh-copy-id tree autojump reattach-to-user-namespace gitup ag htop bat mobile-shell ghi yarn mas z exa fd fzf ack prettyping diff-so-fancy ncdu tldr pyenv mosh gibo
+# fonts
+brew install freetype font-anonymice-nerd-font font-jetbrains-mono-nerd-font
+
+# for development
+brew install pyenv pyenv-virtualenv jenv nvm rustup cmake shellcheck xctool gitup mobile-shell ghi yarn icdiff diff-so-fancy tokei openjdk openssl openssh krb5 imagemagick ios-deploy ideviceinstalle cocoapods gh ghi gibo git-extras git-flow git-lfs git-open git-quick-stats sqlite
+
+# GUI quicklook tools
+brwe install provisionql qlimagesize qlmarkdown qlprettypatch qlvideo quicklook-json webpquicklook fliqlo
+
+# casks
+brew install 1password-cli
+brew install --cask cheatsheet dash eudic muzzle the-unarchiver visual-studio-code warp
 
 # To install useful key bindings and fuzzy completion
 $(brew --prefix)/opt/fzf/install
 
 # setup pyenv
-local pyenv_versions="2.7.17 3.8.2"
+local pyenv_versions="2.7.18 3.10.15"
 pyenv install $pyenv_versions
 pyenv global $pyenv_versions
 
 diff-so-fancy --set-defaults
 
-# Install iTerm2 integrations
-curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
-
-# Upgrade any already-installed formulae.
-#brew upgrade --all
-
 # Remove outdated versions from the cellar.
-#brew cleanup
+brew cleanup
 
-# Install necessary npm packages
-#npm install -g hexo hexo-cli
-
-# Install simiki. for more information please refer to: http://simiki.org
-#pip install -U simiki fabric Pygments
-
-# Install rust through rustup: https://www.rustup.rs/
-curl https://sh.rustup.rs -sSf | sh
