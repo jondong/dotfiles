f33d24991f907b8c552011bf691451d5e48ad0e8
Dong Yan <dongyan09@live.cn>
2025-09-12 01:08:45 +0800

    Update linux install and settings

--- platforms/linux/install.sh
+++ platforms/linux/install.sh
@@ -3,20 +3,13 @@
 read -e -p "Install Linux for production deployment? (Which means do not install packages for devs) [y/N] " -n 1
 is_production=${REPLY:=n}
 
-packages=(vim git tmux autojump xclip ssh tree shellcheck openssl unzip curl tar zsh exfat-utils exfat-fuse nodejs yarn cmake)
+packages=(neovim git autojump xclip ssh tree openssl unzip curl tar zsh exfat-fuse eza bat fd-find tree prettyping pipx findutils ack duf)
 if [ ${is_production,,} = 'n' ]; then
   echo "Install packages for development machine."
-  packages=("${packages[@]}" git-extras terminator mosh ruby ruby-dev source-highlight expect cgdb valgrind clang global cscope exuberant-ctags python-setuptools python-pip cargo net-tools ncdu silversearcher-ag htop gnome-tweak-tool)
+  packages=("${packages[@]}" git-extras git-lfs git-flow gh shellcheck cmake alacritty mosh ruby ruby-dev source-highlight expect cgdb valgrind clang global cscope exuberant-ctags net-tools ncdu gdu ripgrep gnome-tweak-tool rustup btop highlight fonts-jetbrains-mono fonts-firacode fonts-hack rbenv ruby-build direnv docker-compose icdiff)
 else
   echo "Install packages for production machine."
 fi
-
-# Setup nodejs v11 deb source
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-
-# Setup yarn deb source
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Install necessary softwares for Linux here. Also removed unnecessary apps.
 sudo apt -y remove libreoffice-core libreoffice-base-core libreoffice-common
@@ -25,28 +18,30 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install "${packages[@]}"
 
+# Install apps with snap
+snap install --classic zellij
+snap install diff-so-fancy
+
+# Install apps with pipx
+pipx install gitup uv poetry
+
+# Install rust environment and rust apps
+rustup default stable
+cargo install tokei
+
 # Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+[[ ! -d ~/.fzf ]] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-# Install prettyping
-curl --create-dirs -o bin/prettyping https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping
-chmod +x ~/bin/prettyping
+# Install jenv
+[[ ! -d ~/.jenv ]] && git clone https://github.com/jenv/jenv.git ~/.jenv
 
-# Install tldr
-curl --create-dirs -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
-chmod +x ~/bin/tldr
+# Install nvm
+[[ ! -d ~/.nvm ]] && git clone https://github.com/nvm-sh/nvm.git ~/.nvm
 
-# Avoid zsh-compinit-insecure-directories issues.
-# refers to: http://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories for more details.
-pushd /usr/local/share/zsh
-sudo chmod -R 755 ./site-functions
-popd
+# Install lazydocker
+[[ ! -f ~/.local/bin/lazydocker ]] && curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
 if [ ${is_production,,} = 'n' ]; then
-  # Install packages with cargo
-  cargo install exa bat fd-find
-
   # setup crontab
   if [ -f "$HOME/.crontab" ]; then
     crontab -u $(whoami) "$HOME/.crontab"
